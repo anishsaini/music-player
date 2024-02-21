30c33a95f94684a09c90ac26f777f043373a4e86
anishsaini <anishoficial@gmail.com>
2024-02-21 17:34:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let progress = document.getElementById("progress");
+let song = document.getElementById("song");
+let ctRl = document.getElementById("ctRl");
+
+song.onloadedmetadata = function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+};
+function playpause() {
+  if (ctRl.classList.contains("fa-pause")) {
+    song.pause();
+    ctRl.classList.remove("fa-pause");
+    ctRl.classList.add("fa-play");
+  } else {
+    song.play();
+    ctRl.classList.add("fa-pause");
+    ctRl.classList.remove("fa-play");
+  }
+}
+
+if (song.play()) {
+  setInterval(() => {
+    progress.value = song.currentTime;
+  }, 500);
+}
+progress.onchange = function () {
+  song.play();
+  song.currentTime = progress.value;
+  ctRl.classList.add("fa-pause");
+  ctRl.classList.remove("fa-play");
+};
